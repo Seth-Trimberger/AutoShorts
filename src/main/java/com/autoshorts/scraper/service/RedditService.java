3bef1d1924a4863476ef2a4ccced6a85bd78bf46
Seth Trimberger <sethtrimberger@Seths-MacBook-Pro.local>
2026-01-01 00:02:04 -0800

    Worked on the webscraping

---
+++ src/main/java/com/autoshorts/scraper/service/RedditService.java
@@ -0,0 +1,23 @@
+package com.autoshorts.scraper.service;
+
+import com.autoshorts.scraper.Model.Story;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Service;
+
+public class RedditService {
+
+    // Using a "User Agent" makes us look like a real browser so Reddit doesn't block us immediately
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+
+    public Story scrapeStoryFromUrl(String url){
+        try{
+            Story story = new Story();
+            return story;
+        }catch (Exception e){
+            System.err.println("Scrapping failed for: " + url + " | Error: " + e.getMessage());
+            return null;
+        }
+    }
+}
